e2f5828d46773e3b57445375732d90b614a18431
Zhang Chen <18741306@qq.com>
2020-02-04 22:22:46 +0800

    shorten list code

--- products.py
+++ products.py
@@ -4,10 +4,8 @@ while True:
 	if name =='q':
 		break
 	price = input('请输入商品价格：')
-	p = []
-	p.append(name)
-	p.append(price)
-	products.append(p)
-print(products[0][0])
-print(products[0][1])
+#	p = [name,price]
+	products.append([name,price])
+
+print(products)
 
