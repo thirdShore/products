4cee416c5357466fcf173e94519206f772524368
Zhang Chen <18741306@qq.com>
2020-02-05 12:20:07 +0800

    add header

--- products.py
+++ products.py
@@ -11,3 +11,8 @@ print(products)
 for p in products:
 	print(p[0],'的价格是:',p[1])
 
+with open('products.csv','w') as f:
+	f.write('商品,价格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
